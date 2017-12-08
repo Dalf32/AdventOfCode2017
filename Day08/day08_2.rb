5c144a86f847a66b43171f49a6e825931004f2e4
Dalf32 <kylepmullins@gmail.com>
2017-12-08 02:13:42 -0500

    Day 8 solutions

---
+++ Day08/day08_2.rb
@@ -0,0 +1,34 @@
+##
+# day08_2.rb
+#
+# AUTHOR::  Kyle Mullins
+##
+
+def process_input(input_lines)
+  input_lines.map { |instr| instr.chomp.gsub('inc', '+').gsub('dec', '-') }
+end
+
+def solve(input)
+  register_obj = Object.new
+  registers = input.map { |instr| register(instr) }.uniq
+  register_vars = registers.map { |reg| ':' + reg }.join(', ')
+
+  register_obj.singleton_class.instance_eval("attr_accessor #{register_vars}")
+  registers.map { |reg| register_obj.instance_eval("@#{reg} = 0") }
+
+  max_register = 0
+
+  input.each do |instr|
+    register_obj.instance_eval("@#{register(instr)} = #{instr}")
+    new_max = register_obj.instance_variables
+                          .map { |var| register_obj.instance_variable_get(var) }
+                          .max
+    max_register = new_max if new_max > max_register
+  end
+
+  max_register
+end
+
+def register(instr)
+  instr.split.first
+end

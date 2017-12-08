6d1861a81652ad9245db07f8998e20a79be9a609
Dalf32 <kylepmullins@gmail.com>
2017-12-08 02:31:08 -0500

    Day 8 refactoring

--- Day08/day08_1.rb
+++ Day08/day08_1.rb
@@ -9,18 +9,32 @@ def process_input(input_lines)
 end
 
 def solve(input)
+  register_obj = setup_register_obj(input)
+
+  input.each { |instr| execute_instruction(register_obj, instr) }
+  max_register(register_obj)
+end
+
+def register(instr)
+  instr.split.first
+end
+
+def setup_register_obj(input)
   register_obj = Object.new
   registers = input.map { |instr| register(instr) }.uniq
   register_vars = registers.map { |reg| ':' + reg }.join(', ')
 
   register_obj.singleton_class.instance_eval("attr_accessor #{register_vars}")
   registers.map { |reg| register_obj.instance_eval("@#{reg} = 0") }
-  input.each { |instr| register_obj.instance_eval("@#{register(instr)} = #{instr}") }
 
-  register_obj.instance_variables
-              .map { |var| register_obj.instance_variable_get(var) }.max
+  register_obj
 end
 
-def register(instr)
-  instr.split.first
+def execute_instruction(register_obj, instr)
+  register_obj.instance_eval("@#{register(instr)} = #{instr}")
+end
+
+def max_register(register_obj)
+  register_obj.instance_variables
+              .map { |var| register_obj.instance_variable_get(var) }.max
 end
